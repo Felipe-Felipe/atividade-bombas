b1ec18748ecdc5b4369443d9c3fc5f4097461980
Felipe-Felipe <fegeatte2@gmail.com>
2026-05-13 22:11:46 -0300

    alteracao

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,27 @@
 function alertar(mensagem) {
     alert(mensagem);
 }
+
+function acessar() {
+    const email = document.querySelector('input[type="email"]');
+    const senha = document.querySelector('input[text= "text"]');
+
+    let valido = true;
+
+    [email, senha].forEach((campo) => {
+        if (!cqmpo.value.trim()) {
+            campo.style.border = "2px solid red";
+            valido = false;
+        } else {
+         campo.style.border = "2px solid green";
+        }
+});
+
+    if (valido) {
+        window.open("index.html", "_self");
+    }
+    
+
+   
+
+}
