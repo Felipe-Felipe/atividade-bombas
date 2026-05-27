1284a249c96978a832b926d027add5066b61bd2c
Felipe-Felipe <fegeatte2@gmail.com>
2026-05-26 21:57:31 -0300

    atualizado

--- funcoes.js
+++ funcoes.js
@@ -1,26 +1,50 @@
+import bombas from "./todas_bombas.js";
+
 function alertar(mensagem) {
-    alert(mensagem);
+  alert(mensagem);
 }
 
-const email = document.getElementById("email")
-const senha = document.getElementById("senha")
-const form = document.querySelector(".login_form")
+//codigos da tela de login
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const form = document.querySelector(".login_form");
+let registro_senha = "069817";
+let registro_email = "admin@admin";
 
-let registro_senha = "069817" 
-let registro_email = "admin@admin" 
+//codigos da tela do dashboard
+const mensagem_inicio = document.querySelector(".mensagem");
+const consultador = document.querySelector(".pesquisar_bombas");
+const btns_dash = document.querySelectorAll(".btn_dash");
 
+btns_dash.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    console.log(bombas);
+    mudar_dashboard(index);
+  });
+});
 
-form.addEventListener("submit",(e)=>{
-e.preventDefault()
-// console.log(email.value);
-// console.log(senha.value);
-
-if(registro_senha == senha.value && registro_email == email.value )
-{
-    alert("login com sucesso!")
-    window.location.href = "bemvindo.html"
+function mudar_dashboard(item) {
+  switch (item) {
+    case 0:
+      mensagem_inicio.style.display = "block";
+      consultador.style.display = "none";
+      break;
+    case 1:
+      mensagem_inicio.style.display = "none";
+      consultador.style.display = "block";
+      break;
+  }
 }
-else {
-    alert("dados incorretos")
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (registro_senha == senha.value && registro_email == email.value) {
+      alert("login com sucesso!");
+      window.location.href = "bemvindo.html";
+    } else {
+      alert("dados incorretos");
+    }
+  });
 }
-})
---
+++ todas_bombas.js
@@ -0,0 +1,16 @@
+const bombas = [
+  {
+    bomba: "bomba 1",
+    preço: "10",
+  },
+  {
+    bomba: "bomba 2",
+    preço: "12",
+  },
+  {
+    bomba: "bomba 3",
+    preço: "12",
+    
+  },
+];
+export default bombas;
